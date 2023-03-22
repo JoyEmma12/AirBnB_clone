0d6a9af8b1d864aaa909128a5ebd4878f27dfcad
JoyEmma12 <ejoy0600@gmail.com>
2023-03-22 19:30:38 +0000

    0x00 AirBnB clone project

---
+++ models/engine/file_storage.py
@@ -0,0 +1,116 @@
+#!/usr/bin/python3
+import json
+
+class FileStorage:
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """returns dictionary objects"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Sets in objects with key"""
+        key = "{}.{}".format(obj.__class__.name___, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """serializes objects to the json file path"""
+        json_dict = {}
+        for k, v in self.__objects.items():
+            json_dict[k] = v.to_dict()
+        with open(self.__file_path, mode='w', encoding='utf-8') as f:
+            f.write(json.dumps(json_dict))
+ 
+    def classes(self):
+        from models.base_model import BaseModel
+        from models.city import City
+        from models.place import Place
+        from models.state import State
+        from models.amenity import Amenity
+        from models.review import Review
+
+        classes = {
+           "Basemodel": BaseModel,
+           "City": City,
+           "Place": Place,
+           "State": State,
+           "Amenity": Amenity,
+           "Review": Review
+        }
+        return classes
+
+    def reload(self):
+        """deserialize the json file to objects"""
+        if not path.isfile(FileStorage.__file_path):
+            return
+
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+            serialized_objects = json.load(f)
+
+        deserialized_objects = {}
+
+        for obj_id, serialized_obj in serialized_objects.items():
+            class_name = serialized_obj['__class__']
+            if class_name in self.classes():
+                obj_class = self.classes()[class_name]
+            else:
+                continue
+
+            deserialized_obj = obj_class(**serialized_obj)
+            deserialized_objects[obj_id] = deserialized_obj
+
+        FileStorage.__objects = deserialized_objects
+
+    def attributes(self):
+        """returns valid attributes and their type"""
+        attributes = {
+            "BaseModel":
+            {
+                "id": str,
+                "created_at": datetime.datetime,
+                "updated_at": datetime.datetime
+            },
+            "User":
+            {
+                "email": str,
+                "password": str,
+                "first_name": str,
+                "last_name": str
+            },
+            "State":
+            {
+                "name": str
+            },
+            "City":
+            {
+                "state_id": str,
+                "name": str
+            },
+            "Amenity":
+            {
+                "name": str
+            },
+            "Place":
+            {
+                "city_id": str,
+                "user_id": str,
+                "name": str,
+                "description": str,
+                "number_rooms": int,
+                "number_bathroom": int,
+                "max_guest": int,
+                "price_by_night": int,
+                "latittude": float,
+                "longitude": float,
+                "amenity_ids": str
+            },
+            "Review":
+            {
+                "place_id": str,
+                "user_id": str,
+                "text": str
+            }
+        }
+        return attributes
---
+++ tests/test_models/base_model.py
@@ -0,0 +1,36 @@
+#!/isr/bin/python3
+import models
+from datetime import datetime
+from uuid import uuid4
+
+class BaseModel:
+
+    def __init__(self, *args, **kwargs):
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
+
+
+    def __str__(self):
+        return "[{}] ({}) {}".\
+            format(__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.today()
+        models.storage.save()
+
+    def to_dict(self):
+        mydict = self.__dict__.copy()
+        mydict["__class__"] = type(self).__name__
+        mydict["created_at"] = mydict["created_at"].isoformat()
+        mydict["updated_at"] = mydict["updated_at"].isoformat()
+        return mydict
